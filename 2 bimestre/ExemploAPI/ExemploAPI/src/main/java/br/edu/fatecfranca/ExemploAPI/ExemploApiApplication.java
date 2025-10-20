d40e272536203d435860bcc4398a955d17a799dc
ANDREA DE FATIMA BORGES LIMA <1090482411004@fatec-srv.local>
2025-10-20 08:00:17 -0300

    API

---
+++ 2 bimestre/ExemploAPI/ExemploAPI/src/main/java/br/edu/fatecfranca/ExemploAPI/ExemploApiApplication.java
@@ -0,0 +1,21 @@
+package br.edu.fatecfranca.ExemploAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController  //isso não é muito certo, mas como é um exemplo pra nós vermos colocamos ele
+public class ExemploApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApiApplication.class, args);
+	}
+
+    @GetMapping("/mensagem")
+    public String mensagem(){
+        return "Tenha um bom dia";
+    }
+
+}
